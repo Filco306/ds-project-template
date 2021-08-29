7f4f435aefbcdef34ca9d585cb3944569a5f466f
Filip Cornell <c.filip.cornell@gmail.com>
2021-08-29 18:43:27 +0000

    Added some more utils to speed up running of an experiment.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import json
+import argparse
+from utils import fix_filename
+from run_experiment import run
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", default="test.json", type="str")
+
+    args = parser.parse_args()
+    config_file = json.load(open(fix_filename(args.config_file), "r"))
+    print(f"Config: {config_file}")
+    run(config_file)
